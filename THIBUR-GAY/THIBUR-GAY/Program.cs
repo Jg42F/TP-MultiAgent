113074ab110c7ee31de4c39c84434a98a035ccdb
Slyclouds <Slyclouds@hotmail.fr>
2015-10-28 15:24:37 +0100

    Renommage

--- THIBUR-GAY/THIBUR-GAY/Program.cs
+++ THIBUR-GAY/THIBUR-GAY/Program.cs
@@ -16,7 +16,7 @@ namespace THIBUR_GAY
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
