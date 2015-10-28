fa2a1fdc39f7905bf2e3c6e82b461f7286d74c64
Slyclouds <Slyclouds@hotmail.fr>
2015-10-28 17:52:59 +0100

    Ajout des classes agents and co

--- THIBUR-GAY/THIBUR-GAY/MainForm.cs
+++ THIBUR-GAY/THIBUR-GAY/MainForm.cs
@@ -16,5 +16,10 @@ namespace THIBUR_GAY
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
---
+++ THIBUR-GAY/THIBUR-GAY/Parking/Agents/VehiculeAgent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THIBUR_GAY.Agents
+{
+    class Vehicule
+    {
+        protected const double STEP = 3;
+        protected const double DISTANCE_MIN = 5;
+        protected const double DISTANCE_SLOW = 20;
+
+        protected double speedX;
+        public double SpeedX { get { return speedX; } }
+        protected double speedY;
+        public double SpeedY { get { return speedY; } }
+
+        internal
+    }
+}
